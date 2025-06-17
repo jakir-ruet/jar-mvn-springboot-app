1e2d754361241f0efb21e8a82730e46a521ab21d
jakir-ruet <jakir.ruet.bd@gmail.com>
2025-06-17 21:21:00 +0600

    initial commit

---
+++ src/main/java/com/jarmvnspringbootapp/jarmvnspringbootapp/JarMvnSpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.jarmvnspringbootapp.jarmvnspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarMvnSpringbootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JarMvnSpringbootAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jarmvnspringbootapp/jarmvnspringbootapp/JarMvnSpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarmvnspringbootapp.jarmvnspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarMvnSpringbootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
